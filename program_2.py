55418c6b7747b1792cd59aced95949a46ca2e4c4
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2026-07-17 09:06:54 +0300

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,5 +1,19 @@
-import psutil, time
+import subprocess
 
-net = psutil.net_io_counters()
-print(f"Bytes sent: {net.bytes_sent}")
-print(f"Bytes received: {net.bytes_recv}")
+result = subprocess.run(
+    ['sudo', 'dmidecode', '--type', 'memory'],
+    capture_output=True, text=True
+)
+
+stats = {}
+
+for i in result.stdout.splitlines():
+    splitted = i.strip()
+    if ':' in splitted:
+        key, _, value = splitted.partition(':')
+        key = key.strip()
+        value = value.strip()
+        stats[key] = value
+
+for key, value in stats.items():
+    print(key, value)
