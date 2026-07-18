588c5359c059e4924075058bb8a94dc4533b8361
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2026-07-18 16:49:56 +0300

    Create cpu_monitor.py

---
+++ src/monitors/cpu_monitor.py
@@ -0,0 +1,44 @@
+import psutil
+import subprocess
+import time
+from datetime import timedelta
+from PyQt6.QtCore import QObject, QTimer, pyqtSignal
+
+class CPUMonitor(QObject):
+    stats_updated = pyqtSignal(dict)
+
+    def __init__(self, interval_ms=1000, parent=None):
+        super().__init__(parent)
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.poll)
+        self.timer.start(interval_ms)
+
+        self.data = self.get_static()
+    
+    def poll(self):
+        data = {
+            'utilization': psutil.cpu_percent(),
+            'current_speed': psutil.cpu_freq().current / 1000,
+        }
+
+        self.stats_updated.emit(data)
+    
+    def get_static(self):    
+        data = {
+            'uptime': str(timedelta(seconds=int(time.time() - psutil.boot_time()))),
+            'base_speed': psutil.cpu_freq().min / 1000,
+            'psyhical': psutil.cpu_count(logical=False),
+            'logical': psutil.cpu_count(logical=True),
+        }
+
+        result = subprocess.run(
+            ['lscpu'],
+            capture_output=True, text=True
+        )
+        for i in result.stdout.splitlines():
+            if ":" in i:
+                label, value = i.split(':', 1)
+                data[label.strip()] = value.strip()
+        
+        return data
+         
