7ef84912113e8b399dd774de5d0beed7bf5cff9e
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2026-07-22 16:27:41 +0300

    Take back last changes

--- program_2.py
+++ program_2.py
@@ -1,15 +1,5 @@
 import subprocess
 
-result = subprocess.run(
-    ['lscpu'],
-    capture_output=True, text=True
-)
+line_as_bytes = subprocess.check_output("nvidia-smi -L", shell=True)
 
-data1 = dict()
-for i in result.stdout.splitlines():
-    if ":" in i:
-        label, value = i.split(':', 1)
-        data1[label.strip()] = value.strip()
-
-for i, v in data1.items():
-    print(i)
+print(line_as_bytes)
